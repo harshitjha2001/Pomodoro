97940fe904fa03adbef87973804135171755ff39
harshitjha2001 <85453454+harshitjha2001@users.noreply.github.com>
2021-11-27 14:11:16 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,24 +1,24 @@
-var start =document.getElementById('start');
-var reset=document.getElementById('reset');
-var stop =document.getElementById('pause');
+var start = document.getElementById('start');
+var reset = document.getElementById('reset');
+var stop = document.getElementById('pause');
 
-var wm=document.getElementById('w_minutes');
-var ws=document.getElementById('w_seconds');
+var wm = document.getElementById('w_minutes');
+var ws = document.getElementById('w_seconds');
 
-var bm=document.getElementById('b_minutes');
-var bs=document.getElementById('b_seconds');
+var bm = document.getElementById('b_minutes');
+var bs = document.getElementById('b_seconds');
 
 var startTimer;
 
-start.addEventListener('click', function(){
-    if(startTimer === undefined){
+start.addEventListener('click', function () {
+    if (startTimer === undefined) {
         startTimer = setInterval(timer, 1000)
     } else {
         alert("Timer is already running");
     }
 })
 
-reset.addEventListener('click', function(){
+reset.addEventListener('click', function () {
     wm.innerText = 25;
     ws.innerText = "00";
 
@@ -30,31 +30,39 @@ reset.addEventListener('click', function(){
     startTimer = undefined;
 })
 
-stop.addEventListener('click', function(){
+stop.addEventListener('click', function () {
     stopInterval()
     startTimer = undefined;
 })
 
-function timer()
-{
-    if(ws.innerText != 0){
+function timer() {
+    if (ws.innerText != 0) {
         ws.innerText--;
-    } else if(wm.innerText != 0 && ws.innerText == 0){
+    } else if (wm.innerText != 0 && ws.innerText == 0) {
         ws.innerText = 59;
         wm.innerText--;
     }
 
-    if(wm.innerText == 0 && ws.innerText == 0){
-        if(bs.innerText != 0){
+    
+    if (wm.innerText == 0 && ws.innerText == 0)
+       {
+        if( bm.innerText == 5 && bs.innerText == 00)
+        {
+            alert('session over ! please take rest');
+        }
+        if (bs.innerText != 0) {
             bs.innerText--;
-        } else if(bm.innerText != 0 && bs.innerText == 0){
+            
+        } else if (bm.innerText != 0 && bs.innerText == 0) {
             bs.innerText = 59;
             bm.innerText--;
         }
     }
-   
 
-    if(wm.innerText == 0 && ws.innerText == 0 && bm.innerText == 0 && bs.innerText == 0){
+
+
+    if (wm.innerText == 0 && ws.innerText == 0 && bm.innerText == 0 && bs.innerText == 0) {
+        alert('break over! start working again');
         wm.innerText = 25;
         ws.innerText = "00";
 
@@ -63,8 +71,8 @@ function timer()
 
         document.getElementById('counter').innerText++;
     }
-}    
+}
 
-function stopInterval(){
+function stopInterval() {
     clearInterval(startTimer);
 }
